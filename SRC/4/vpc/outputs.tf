812891acdd28de17990f59cea3734711d0e920ce
Igor Panarin <59224857+networksuperman@users.noreply.github.com>
2023-12-19 14:11:54 +0300

    Delete SRC directory

--- SRC/4/vpc/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "network_id" {
-  value = yandex_vpc_network.vpc_net.id
-  }
-output "subnet_id" {
-  value = yandex_vpc_subnet.vpc_subnet["ru-central1-a"].id
-  }
-output "vpc_subnet" {
-  value = yandex_vpc_subnet.vpc_subnet[*]
-  }
